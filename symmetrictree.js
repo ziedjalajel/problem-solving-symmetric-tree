3d72cda8ad57d1c782303879320e97b17d076944
ziedjalajel <zied.jalajel1993@gmail.com>
2021-06-21 22:58:55 +0300

    A meaningful message

--- symmetrictree.js
+++ symmetrictree.js
@@ -9,7 +9,7 @@ const root = new Node(1);
 root.left = new Node(2);
 root.right = new Node(2);
 root.left.left = new Node(3);
-root.left.right = new Node(1);
+root.left.right = new Node(4);
 root.right.left = new Node(4);
 root.right.right = new Node(3);
 
